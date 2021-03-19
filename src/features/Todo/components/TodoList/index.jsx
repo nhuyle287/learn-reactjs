5c51da4e7f9020cd41fec2e9bfbfbdf26919347f
nhuyle287 <50352152+nhuyle287@users.noreply.github.com>
2021-03-19 16:47:43 +0700

    state todolist

--- src/features/Todo/components/TodoList/index.jsx
+++ src/features/Todo/components/TodoList/index.jsx
@@ -1,20 +1,45 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import classNames from 'classnames';
+import "./styles.scss"
 
 TodoList.propTypes = {
-    todoList:PropTypes.array,
+    todoList: PropTypes.array,
+    onTodoClick: PropTypes.func,
 };
 
-TodoList.defaultProps={
-    todoList:[],
+TodoList.defaultProps = {
+    todoList: [],
+    onTodoClick: null,
 }
 
+
+
 function TodoList(props) {
-    const {todoList}=props;
+    // console.log(props)
+    // nhận todolist và ontodolist từ props truyền qua
+    const { todoList } = props;
+    const { onTodoClick } = props;
+    // gửi todo với index qua todolist
+    const hangletodoList = (todo, idx) => {
+        if (!onTodoClick) return;
+        onTodoClick(todo, idx);
+    }
     return (
-        <ul>
-            {todoList.map(todo=>(
-                <li key={todo.id}>{todo.title}</li>
+        <ul className="todo-list">
+            {/* Lấy danh sách todo và index để bk vtri cap nhat trang thái
+                Khi dung map phải truyền key
+                cài classNames để add class
+                classNames({danh sách class})
+            */}
+            {todoList.map((todo, idx) => (
+                <li key={todo.id}
+                    className={classNames({
+                        "todo-item": true,
+                        completed: todo.status === "completed"
+                    })}
+                    onClick={() => hangletodoList(todo, idx)}
+                >{todo.title}</li>
             ))}
         </ul>
     );
