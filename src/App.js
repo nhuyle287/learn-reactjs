e43abb5a25666bbbadaaff9e2c47c0179a796528
nhuyle287 <50352152+nhuyle287@users.noreply.github.com>
2021-03-20 16:06:09 +0700

    router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import React from "react";
-// import AlbumFeatue from "./features/Song";
+import { Route, Link, Switch } from "react-router-dom";
+import AlbumFeatue from "./features/Song";
 import TodoList from "./features/Todo";
 
 function App() {
   // return <AlbumFeatue />;
-  return <TodoList />;
+  return (
+    <div className="app">
+      <h3>Homepage</h3>
+
+      <p>
+        <Link to="todoList">TodoList</Link>
+      </p>
+      <p>
+        <Link to="album">AlbumFeatue</Link>
+      </p>
+      <Switch>
+        <Route path="/todoList" component={TodoList} exact />
+        <Route path="/album" component={AlbumFeatue} exact/>
+      </Switch>
+      {/* <TodoList /> */}
+    </div>
+  );
 }
 
 export default App;
--- src/features/Todo/index.jsx
+++ src/features/Todo/index.jsx
@@ -2,49 +2,75 @@ import React, { useState } from 'react';
 // import PropTypes from 'prop-types';
 import TodoList from './components/TodoList/index.jsx'
 // TodoFeature.propTypes = {
-    
+
 // };
 
 function TodoFeature(props) {
-    const inittodoList=[
+    const inittodoList = [
         {
-            id:1,
-            title:'eat',
-            status:'new',
+            id: 1,
+            title: 'eat',
+            status: 'new',
         },
         {
-            id:2,
-            title:'eat2',
-            status:'completed',
+            id: 2,
+            title: 'eat2',
+            status: 'completed',
         },
         {
-            id:3,
-            title:'eat3',
-            status:'new',
+            id: 3,
+            title: 'eat3',
+            status: 'new',
         }
     ];
 
     const [todoList, settodoList] = useState(inittodoList);
+    // Mặc định là all
+    const [filterStatus,setfilterSattus]=useState('all');
 
-    const hangleTodoList=(todo,idx)=>{
-            // console.log(todo,idx);
-            // tạo một todolist mới = todolist
-            const newTodoList=[...todoList];
-            // Lấy cái todo với giá trị index truyền vào cập nhật lại trạng thái
-            const newTodo={
-                ...newTodoList[idx],
-                status:newTodoList[idx].status==="new"?"completed":"new",
-            }
-            // todolist với gtri index bắng newtodo
-            newTodoList[idx]=newTodo;
-            settodoList(newTodoList);
+
+    const hangleTodoList = (todo, idx) => {
+        // console.log(todo,idx);
+        // tạo một todolist mới = todolist
+        const newTodoList = [...todoList];
+        // Lấy cái todo với giá trị index truyền vào cập nhật lại trạng thái
+        const newTodo = {
+            ...newTodoList[idx],
+            status: newTodoList[idx].status === "new" ? "completed" : "new",
+        }
+        // todolist với gtri index bắng newtodo
+        newTodoList[idx] = newTodo;
+        settodoList(newTodoList);
     }
+
+    const hangleShowClick=()=>{
+        setfilterSattus('all');
+    }
+    const hangleShowCompletedClick=()=>{
+        setfilterSattus('completed');
+    }
+    const hangleShowNewClick=()=>{
+        setfilterSattus('new');
+    }
+    // Lọc theo status
+    const filterTodoList=todoList.filter((todo) => filterStatus==='all'||filterStatus===todo.status);
+    // console.log(filterTodoList)
 
     return (
         <div>
             <h3>Todo List</h3>
-            {/*cha có một cái todolist khi nào nhấn nút thì thực hiện hàm hangletodolist và reder todolist */}
-            <TodoList todoList={todoList} onTodoClick={hangleTodoList} />
+            {/*cha có một cái todolist khi nào nhấn nút thì thực
+             hiện hàm hangletodolist và reder todolist */}
+            <TodoList todoList={filterTodoList} onTodoClick={hangleTodoList} />
+
+
+
+            <div>
+                <button onClick={hangleShowClick}>ShowAll</button>
+                <button onClick={hangleShowCompletedClick}>Show Completed</button>
+                <button onClick={hangleShowNewClick}>Show New</button>
+            </div>
+
         </div>
     );
 }
